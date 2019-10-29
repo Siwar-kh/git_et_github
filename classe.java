5dde7253f717856eef21c133407a785a06ce27cb
siwarKHLIFI <siwarmaster16@gmail.com>
2019-10-29 04:49:21 -0400

    Amélioration : ajout d'une condition else if pour i = 0

--- classe.java
+++ classe.java
@@ -1,6 +1,10 @@
 int i = 0
+
 if (i < 0)
 	system.out.println("ce nombre est négatif");
+
+else if (i = 0)
+	system.out.println("ce nombre est nul");
 
 else 
 	system.out.println("ce nombre est positif");
